250a9c6fcd19f7defdb5751655678e130b9201c0
coding-nerfilio <frisso2001@gmail.com>
2022-05-12 03:10:46 -0300

    Svg Support | Add Chakra ui | Small fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
+import { Container } from "@chakra-ui/react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/Navbar";
 import ExchangerPage from "./pages/ExchangerPage";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<LandingPage />} />
-			<Route path="/exchange" element={<ExchangerPage />} />
-			<Route path="*" element={<Navigate to="/" />} />
-		</Routes>
+		<Container p={0} m={0} maxW={"100vw"} w="100vw">
+			<NavBar />
+
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/exchange" element={<ExchangerPage />} />
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		</Container>
 	);
 }
 
